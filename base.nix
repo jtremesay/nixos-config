c863f23728bc9183125f10a425c8eada5a001425
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-11-04 22:47:33 +0100

    don't ask for sudo password

--- base.nix
+++ base.nix
@@ -11,6 +11,14 @@
       ./services.nix
     ];
   
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "22.05"; # Did you read the comment?
+  
   # Copy the NixOS configuration file and link it from the resulting system
   # (/run/current-system/configuration.nix). This is useful in case you
   # accidentally delete configuration.nix.
@@ -29,11 +37,6 @@
     "flakes"
   ];
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "22.05"; # Did you read the comment?
+  # Disable passowrd for sudo
+  security.sudo.wheelNeedsPassword = false;
 }
