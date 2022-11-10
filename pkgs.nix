1340dd370205f2dcd74b2cecf1edb4c376e6d91f
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-11-10 15:40:56 +0100

    add psmisc (killall)

--- pkgs.nix
+++ pkgs.nix
@@ -1,27 +1,28 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  environment.systemPackages = with pkgs; [ bind curl httpie jq nixfmt ];
+  environment.systemPackages = with pkgs; [ bind curl httpie jq nixfmt psmisc ];
 
-  programs.fish.enable = true;
-  programs.git.enable = true;
-  programs.gnupg.agent = {
-    enable = true;
-    enableSSHSupport = true;
-  };
-  programs.htop = {
-    enable = true;
-    settings = {
-      hide_kernel_threads = true;
-      hide_userland_threads = true;
+  programs = {
+    fish.enable = true;
+    git.enable = true;
+    gnupg.agent = {
+      enable = true;
+      enableSSHSupport = true;
     };
+    htop = {
+      enable = true;
+      settings = {
+        hide_kernel_threads = true;
+        hide_userland_threads = true;
+      };
+    };
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+      viAlias = true;
+      vimAlias = true;
+    };
+    tmux.enable = true;
   };
-
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-    viAlias = true;
-    vimAlias = true;
-  };
-  programs.tmux.enable = true;
 }
