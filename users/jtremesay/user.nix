1e6ee16b171ede6265805cbcc9d2f083e1e58b7a
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-11-17 23:22:36 +0100

    move users to base

--- users/jtremesay/user.nix
+++ users/jtremesay/user.nix
@@ -5,18 +5,6 @@
 {
   imports = [ <home-manager/nixos> ];
 
-  users.users.jtremesay = {
-    isNormalUser = true;
-    extraGroups = [ "audio" "networkmanager" "wheel" ];
-    useDefaultShell = true;
-  };
-
-  users.extraUsers.jtremesay = {
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC2QfuCeW4Pv+fZnlukbg844Z2P67m4ImBvdyNL7bPXdxH1FoAlWEbZpTvvZPXWNzHnrAQ63GEN07a0bzlbzG0ut/xHKBx/ltZcjcNQkQGB5ta2mewfKCQM/nAA2VdhwbZCa5VGJyCq3oj9HKaJ5RTHsSOSIvh7FQRbM/uAKUZHtfLzd7jBWwuyB/VSB1qSEO4yCHtYjvO4eEjur86fZNe+pHsejtuFN5HMh5QVMeud0VmbeB93L3M2die76u0Nd/Ea9q7/FkR4xR8a9OXAmp7PmCoB7/UlKufh6KvGeHE8xKl8Pibf/cpR56ViRvav660+TREQ9uxoi81S9r4qCOUUEEsuAIaVmGTsKeZ1zSWG3PGwTtIgMOxpCXF1FhSHcIvMh4JNkykzdG6Ul76kDEw6Tdc2cE6uA+o7jnVo3TDrSM5ZYE1ZrRlCz1SWRspx04GUxrjWNTd3wkiAx6vliKMM9/2FdzZRgDk4hlbQgoi1jO7TK/KZQKTaWBbIyTDgGNXQZjhM5fwW2LrmLymD6OXJo4qGLZwD51X+TX0JJZrxmUlsTNQE852o0snEl/rqojsp0+dn3x3Zti868kINTNF0PNcO/HgqwLqk67K3JcFlJNV+sVshUtW7gjHaqZ+Fpe4vV5mECQ2qrSw/ulr8EmeMaPsve9hltE2qnYSZaQSq0Q== killruana@edemaruh.slaanesh.org"
-    ];
-  };
-
   home-manager.users.jtremesay = { pkgs, ... }: {
     nixpkgs.config.allowUnfree = true;
     home = {
