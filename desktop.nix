5a83e0d4fe1db8d7bb13b4b3c5f053af4debb5fd
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-11-17 23:34:44 +0100

    merge nixpkg config

--- desktop.nix
+++ desktop.nix
@@ -4,20 +4,8 @@
   # Use network manager
   networking.networkmanager.enable = true;
 
-  # Modify Steam package:
-  #  - add missing libraries for some games
-  nixpkgs = {
-    config.allowUnfree = true;
-    overlays = [
-      (self: super: {
-        steam =
-          super.steam.override { extraPkgs = pkgs: with pkgs; [ libpng icu ]; };
-      })
-    ];
-  };
-
   # It would be better to have steam enabled per user
-  programs.steam = { enable = true; };
+  programs.steam.enable = true;
 
   hardware = {
     bluetooth.enable = true;
@@ -26,7 +14,17 @@
       support32Bit = true;
     };
   };
-  nixpkgs.config.pulseaudio = true;
+  nixpkgs = {
+    config = {
+      allowUnfree = true;
+      pulseaudio = true;
+    };
+    overlays = [
+      (self: super: {
+        steam = super.steam.override { extraPkgs = pkgs: with pkgs; [ libpng icu ]; };
+      })
+    ];
+  };
   services = {
     # Enable the X11 windowing system.
     xserver = {
