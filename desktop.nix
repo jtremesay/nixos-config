357ae0d09c7e3bdc5d5da482e37e9db3a87bac4e
Jonathan Tremesaygues <jonathan.tremesaygues@slaanesh.org>
2022-11-17 23:30:36 +0100

    inline desktop subfiles

--- desktop.nix
+++ desktop.nix
@@ -1,8 +1,6 @@
 { config, lib, pkgs, modulesPath, ... }:
 
 {
-  imports = [ ./x.nix ./sound.nix ./bluetooth.nix ];
-
   # Use network manager
   networking.networkmanager.enable = true;
 
@@ -20,4 +18,40 @@
 
   # It would be better to have steam enabled per user
   programs.steam = { enable = true; };
+
+  hardware = {
+    bluetooth.enable = true;
+    pulseaudio = {
+      enable = true;
+      support32Bit = true;
+    };
+  };
+  nixpkgs.config.pulseaudio = true;
+  services = {
+    # Enable the X11 windowing system.
+    xserver = {
+      enable = true;
+
+      # Configure X keyboard
+      layout = "fr";
+      xkbVariant = "bepo";
+
+      # Enable touchpad support (enabled default in most desktopManager).
+      libinput.enable = true;
+
+      # Display manager
+      displayManager = {
+        autoLogin = {
+          enable = true;
+          user = config.users.users.jtremesay.name;
+        };
+        sddm.enable = true;
+        session = [{
+          manage = "desktop";
+          name = "XSession";
+          start = "exec $HOME/.xsession";
+        }];
+      };
+    };
+  };
 }
